d9b7cad688d0ddc3eacfacc5ab8d42eea77db44f
Wiltzsu <william.lonnberg@gmail.com>
2024-02-29 21:47:00 +0200

    html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson import ObjectId
 from db_config import MONGO_URI
@@ -27,6 +27,42 @@ def index():
         print(e)
     finally:
         client.close()
+
+@app.route('/items')
+def show_items():
+    client = MongoClient(MONGO_URI)
+    db = client.inventory
+    try:
+        items_cursor = db.item.find().sort('_id', -1)
+        items = list(items_cursor)
+    except Exception as e:
+        print(e)
+        items = []  # Provide an empty list in case of an error
+    finally:
+        client.close()
+
+    return render_template('items.html', items=items)
+
+
+@app.route('/bulk-action', methods=['POST'])
+def handle_bulk_action():
+    action = request.form.get('action')
+    item_ids = request.form.getlist('item_ids')  # Get list of selected item IDs
+
+    if action == 'delete':
+        try:
+            client = MongoClient(MONGO_URI)
+            db = client.inventory
+            for item_id in item_ids:
+                db.item.delete_one({'_id': ObjectId(item_id)})
+        except Exception as e:
+            print(e)
+        finally:
+            client.close()
+
+    # Redirect back to the items list or handle other actions accordingly
+    return redirect(url_for('show_items'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
