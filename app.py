3fffb1646e99eb5e6a1261e8f5b9ba1db89a5e34
Wiltzsu <william.lonnberg@gmail.com>
2024-03-01 17:23:54 +0200

    add items

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def show_items():
 
     return render_template('items.html', items=items)
 
-
+# Delete an item
 @app.route('/bulk-action', methods=['POST'])
 def handle_bulk_action():
     action = request.form.get('action')
@@ -62,6 +62,37 @@ def handle_bulk_action():
 
     # Redirect back to the items list or handle other actions accordingly
     return redirect(url_for('show_items'))
+
+# Route for adding an item, which accepts GET and POST requests
+@app.route('/add-item', methods=['GET', 'POST'])
+def add_item():
+    # Check if current request is a POST requests (form submission)
+    if request.method == 'POST':
+        try:
+            client = MongoClient(MONGO_URI) # Establish database connection
+            db = client.inventory # Access 'inventory' database
+
+            # Create a new item document from the form data
+            new_item = {
+                'name': request.form['name'],
+                'description': request.form['description'],
+                'quantity': int(request.form['quantity']),
+                'categoryId': request.form['categoryId'],
+                'supplierId': request.form['supplierId']
+            }
+
+            # Insert the new item into the database
+            db.item.insert_one(new_item)
+
+            # Redirect to a different page, e.g., the list of items, on success
+            return redirect(url_for('show_items'))
+        except Exception as e:
+            print(e)
+            # Handle the error, possibly showing a user-friendly message
+        finally:
+            client.close()
+    # If it's a GET request, just render the empty form
+    return render_template('add_item.html')
 
 
 if __name__ == '__main__':
